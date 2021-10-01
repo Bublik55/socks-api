9187ba579beebdba454cda3ee1e46cb1de32aea5
Mikhail Krutov <user211195@gmail.com>
2021-10-02 00:22:44 +0600

    exception handler/ route api/socks now returning count of socks

--- src/main/java/com/controller/SockController.java
+++ src/main/java/com/controller/SockController.java
@@ -7,25 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @Validated
 @RequestMapping("/api/socks")
@@ -35,9 +34,10 @@ public class SockController {
   SockRepository sockRepository;
 
   @GetMapping()
-  ResponseEntity<List<Sock>> getByCondition(@RequestParam @NotBlank String color,
+  ResponseEntity<String> getByCondition(@RequestParam @NotBlank String color,
       @RequestParam @Min(0) @Max(100) int cotton_part, @RequestParam @NotBlank String operation) {
     try {
+      color = color.trim();
       List<Sock> socks = new ArrayList<Sock>();
       switch (operation) {
         case ("equal"):
@@ -54,7 +54,8 @@ public class SockController {
       }
       if (socks.isEmpty())
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-      return new ResponseEntity<>(socks, HttpStatus.OK);
+        final Integer toreturn = socks.stream().mapToInt(Sock::getQuantity).sum();
+      return new ResponseEntity<>(Integer.toString(toreturn), HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -64,6 +65,7 @@ public class SockController {
   @PostMapping("/income")
   ResponseEntity<Sock> income(@RequestParam @NotBlank String color, @RequestParam @Min(0) @Max(100) int cotton_part,
       @RequestParam @Min(1) int quantity) {
+    color = color.trim();
     Optional<Sock> sockData = this.sockRepository.findOneByColorAndCottonPart(color, cotton_part);
     if (sockData.isPresent()) {
       Sock _sock = sockData.get();
@@ -79,6 +81,8 @@ public class SockController {
   @PostMapping("/outcome")
   ResponseEntity<Sock> outcome(@RequestParam @NotBlank String color, @RequestParam @Min(0) @Max(100) int cotton_part,
       @RequestParam @Min(1) int quantity) {
+    color = color.trim();
+
     Optional<Sock> sockData = this.sockRepository.findOneByColorAndCottonPart(color, cotton_part);
     if (sockData.isPresent()) {
       Sock _sock = sockData.get();
@@ -87,7 +91,13 @@ public class SockController {
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
       return new ResponseEntity<>(this.sockRepository.save(_sock), HttpStatus.OK);
     } else {
-      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
   }
+
+  @ExceptionHandler({ ConstraintViolationException.class })
+  public ResponseEntity<Object> handleException(Exception exc) {
+    return new ResponseEntity<>(exc, HttpStatus.BAD_REQUEST);
+  }
+
 }
