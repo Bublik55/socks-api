af4c1ebfe7fb1ceb4c46f63f95bbb2bbf9bddaa5
Mikhail Krutov <user211195@gmail.com>
2021-10-15 07:45:14 +0600

    fix models renamed CottonPart to Composite

--- src/main/java/com/home/sock/repository/SockRepository.java
+++ src/main/java/com/home/sock/repository/SockRepository.java
@@ -12,18 +12,18 @@ public interface SockRepository extends JpaRepository<Sock, Long> {
 
   @Query("SELECT sum(s.quantity) FROM Sock s " +
           "INNER JOIN Color c ON s.color = c.id " +
-          "INNER JOIN CottonPart cp ON s.cottonPart = cp.id " +
-          "WHERE c.color.color = ?1 AND cp.cottonPart.cottonPart < ?2 ")
+          "INNER JOIN Composite cp ON s.cottonPart = cp.id " +
+          "WHERE c.color.color = ?1 AND cp.Composite.cottonPart < ?2 ")
   Integer findByColorAndCottonPartLessThan(String color, int cottonPart);
   @Query("SELECT sum(s.quantity) FROM Sock s " +
           "INNER JOIN Color c ON s.color = c.id " +
-          "INNER JOIN CottonPart cp ON s.cottonPart = cp.id " +
-          "WHERE c.color.color = ?1 AND cp.cottonPart.cottonPart > ?2 ")
+          "INNER JOIN Composite cp ON s.cottonPart = cp.id " +
+          "WHERE c.color.color = ?1 AND cp.Composite.cottonPart > ?2 ")
   Integer findByColorAndCottonPartGreaterThan(String color, int cottonPart);
   @Query("SELECT sum(s.quantity) FROM Sock s " +
           "INNER JOIN Color c ON s.color = c.id " +
-          "INNER JOIN CottonPart cp ON s.cottonPart = cp.id " +
-          "WHERE c.color.color = ?1 AND cp.cottonPart.cottonPart = ?2")
+          "INNER JOIN Composite cp ON s.cottonPart = cp.id " +
+          "WHERE c.color.color = ?1 AND cp.Composite.cottonPart = ?2")
   Integer findByColorAndCottonPartEquals(String color, int cottonPart);
 
   Optional<Sock> findOneByColorAndCottonPart(Color color, CottonPart cotton_part);
